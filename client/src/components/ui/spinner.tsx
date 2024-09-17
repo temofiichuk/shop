83455418f1a33688a71c7f15fa8bd94c7b333b51
mihail <mihail.temofiichuk@gmail.com>
2024-09-17 19:45:13 +0200

    ui components

--- client/src/components/ui/spinner.tsx
+++ client/src/components/ui/spinner.tsx
@@ -1,20 +1,24 @@
 import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
 
-export const LoadingSpinner = ({ className }) => {
-	return <span className={"animate-in fade-in duration-1000 "}>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			className={cn("animate-spin", className)}
-		>
-			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
-		</svg>
-	</span>;
+interface Props extends HTMLAttributes<HTMLOrSVGElement> {
+	widths?: number;
+}
+
+export const LoadingSpinner = ({ className, widths, width, height, ...props }: Props) => {
+	return <svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={widths ?? width ?? "24"}
+		height={widths ?? height ?? "24"}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		{...props}
+		className={cn("animate-spin", className)}
+	>
+		<path d="M21 12a9 9 0 1 1-6.219-8.56" />
+	</svg>;
 };
